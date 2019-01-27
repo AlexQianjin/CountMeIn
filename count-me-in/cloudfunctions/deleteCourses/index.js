272d1d1d9b16967034d06c203a6934b5d02e2991
Alex Qin <qianjin.qin@qq.com>
2019-01-27 12:07:43 +0800

    add role and miniGroup propertier to memberInfo and display miniGroup

---
+++ count-me-in/cloudfunctions/deleteCourses/index.js
@@ -0,0 +1,17 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk');
+
+cloud.init();
+
+const db = cloud.database();
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+	try {
+		return await db.collection('courses').where({
+      year: 2019
+    }).remove();
+	} catch (e) {
+		console.error(e);
+	}
+};
